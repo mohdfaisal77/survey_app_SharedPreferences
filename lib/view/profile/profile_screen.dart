52f0cf6d97c80ad4e637ec3b77e9d8f5ff3e7ceb
Mohd Faisal <mohdfaisal7839@gmail.com>
2024-12-21 03:41:47 +0530

    test1 done

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -15,7 +15,6 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
         actions: [
